ffa661850fc11dc81d9c87ce441be2c3803a7e39
spencel <spence.lank.sd@gmail.com>
2018-05-02 15:30:23 -0700

    m

---
+++ meteor-framework/simple-todos/imports/ui/Task.js
@@ -0,0 +1,10 @@
+import React from 'react';
+ 
+// Task component - represents a single todo item
+export default class Task extends React.Component {
+  render() {
+    return (
+      <li>{ this.props.task.text }</li>
+    );
+  }
+}
