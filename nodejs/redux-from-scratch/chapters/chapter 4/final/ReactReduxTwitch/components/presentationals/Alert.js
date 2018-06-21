179b373db1a0c4d446ec325d8e59d995f9d262ac
spencel <spence.lank.sd@gmail.com>
2018-06-21 15:01:55 -0700

    m

---
+++ nodejs/redux-from-scratch/chapters/chapter 4/final/ReactReduxTwitch/components/presentationals/Alert.js
@@ -0,0 +1,35 @@
+<<<<<<< HEAD
+import React from 'react';
+
+//Presentational React Component
+class Alert extends React.Component {
+  componentDidMount () {
+    alert(this.props.error);
+  }
+
+  render() {
+      return (
+        <div>
+        </div>
+      )
+    }
+  }
+export default Alert
+=======
+import React from 'react';
+
+//Presentational React Component
+class Alert extends React.Component {
+  componentDidMount () {
+    alert(this.props.error);
+  }
+
+  render() {
+      return (
+        <div>
+        </div>
+      )
+    }
+  }
+export default Alert
+>>>>>>> b4094a4fa9e55bd6c362c1cfbb0fdf8199fa870f
